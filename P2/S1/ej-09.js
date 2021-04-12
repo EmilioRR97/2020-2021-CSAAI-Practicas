3119da6ab19de448c2d33d9fea3849dac779a785
EmilioRR97 <rosaya.97@gmail.com>
2021-04-12 20:23:28 +0200

    Ejemplo 9 y añado botón

--- P2/S1/ej-09.js
+++ P2/S1/ej-09.js
@@ -1,14 +1,17 @@
-//-- Leer el elemento párrafo test2
+const test3 = document.getElementById('test3')
+
 const test2 = document.getElementById('test2')
 
-//-- Obtener el elemento párrafo 1 para modificarlo
 const test1 = document.getElementById('test1')
 
 //-- Configurar el manejador para el evento de
 //-- pulsación de botón
-test2.onclick = () => {
+test2.onclick = function () {
   console.log("Click sobre el párrafo 2...")
-
-  //-- Cambiar su texto
   test1.innerHTML = "¡TEXTO CAMBIADO!"
+}
+
+test3.onclick = function () {
+    console.log("Click sobre el botón...")
+    test2.innerHTML = "Ahora no puedes ver lo de antes jeje"
 }
