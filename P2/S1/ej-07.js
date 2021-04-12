f2a292bb55bfa250173c52811f717b108a9ec857
EmilioRR97 <rosaya.97@gmail.com>
2021-04-12 20:09:16 +0200

    Termino de arreglar ejemplo 7 modificado

--- P2/S1/ej-07.js
+++ P2/S1/ej-07.js
@@ -1,17 +1,12 @@
 console.log("Ejecutando js...")
 
-
-
-console.log("Párrafo test leido. Dice:")
-console.log(test.innerHTML)
-
 //-- Leer el párrafo identificado como test
 const test = document.getElementById('test')
-console.log("Párrafo test leido. Dice:")
-console.log(test.innerHTML)
-
 //-- Configurar el manejador para el evento de
 //-- pulsación de botón
 test.onclick = function () {
   console.log("Click sobre el párrafo...")
+  console.log("El párrafo dice: ")
+  console.log(test.innerHTML)
+
 }
