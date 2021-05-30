a96ae5b98d431b6099b6a8919bc22e16a52b2fa3
EmilioRR97 <rosaya.97@gmail.com>
2021-05-30 17:45:30 +0200

    Modifico HTML y añado canvas5 JS

---
+++ P3/S6/canvas5.js
@@ -0,0 +1,20 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = 300;
+canvas.height = 100;
+
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+
+    ctx.arc(100, 50, 10, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'yellow';
+
+
+    ctx.stroke()
+    ctx.fill()
+ctx.closePath();
