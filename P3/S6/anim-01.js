eaf83671cc3c1940c104f7635a4f387e8858e573
EmilioRR97 <rosaya.97@gmail.com>
2021-05-30 22:27:45 +0200

    Empiezo con las animaciones, ejemplo 8 HTML JS y CSS

---
+++ P3/S6/anim-01.js
@@ -0,0 +1,31 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = 300;
+canvas.height = 100;
+
+const ctx = canvas.getContext("2d");
+
+let x = 0;
+let y = 0;
+
+function update() 
+{
+  console.log("test");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    ctx.fillStyle = 'red';
+
+    ctx.fill();
+
+    ctx.stroke()
+  ctx.closePath();
+
+  requestAnimationFrame(update);
+}
+
+update();
