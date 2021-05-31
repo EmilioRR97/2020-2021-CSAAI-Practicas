85ca9310d54865e8c5de1501ec5f8921a0272ece
EmilioRR97 <rosaya.97@gmail.com>
2021-05-31 17:00:37 +0200

    Dandole funcionamiento al boton de gris

--- P4/L11/Ej-05.js
+++ P4/L11/Ej-05.js
@@ -5,6 +5,9 @@ const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
+const gris = document.getElementById('gris');
+const decolores = document.getElementById('decolores');
+
 
 const deslizadorR = document.getElementById('deslizadorR');
 const deslizadorG = document.getElementById('deslizadorG');
@@ -15,6 +18,7 @@ const range_valueR = document.getElementById('range_valueR');
 const range_valueG = document.getElementById('range_valueG');
 const range_valueB = document.getElementById('range_valueB');
 
+GRIS: false,
 
 img.onload = function () {
 
@@ -52,6 +56,22 @@ function color() {
   ctx.putImageData(imgData, 0, 0);
 }
 
+function todogris() {
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    ctx.drawImage(img, 0,0);
+
+    for (let i = 0; i < data.length; i+=4) {
+        gr = (3*data[i] + 4*data[i+1]+ data[i+2])/8
+        data[i] = data[i+1] = data[i+2] = gr; 
+      }
+
+  ctx.putImageData(imgData, 0, 0);
+
+}
+
+if (GRIS = false){
+
 deslizadorR.oninput = () => {
     color();
 }
@@ -62,6 +82,12 @@ deslizadorG.oninput = () => {
 
 deslizadorB.oninput = () => {
     color();
+}
+}
+
+gris.onclick = () => {
+    todogris();
+    GRIS = true
 }
 
 console.log("Fin...");
