629343b58aafec0e616647a2887b3def3022cbc3
EmilioRR97 <rosaya.97@gmail.com>
2021-06-01 20:41:33 +0200

    Actualizo con varios cambios y me voy a cenar

--- P3/L8/Ej-01.js
+++ P3/L8/Ej-01.js
@@ -14,9 +14,23 @@ let anchoBase = 80;
 let posicionBase = (canvas.width-anchoBase)/2;
 var pulsacionDerecha = false;
 var pulsacionIzquierda = false;
-
+var filasLadrillos = 5;
+var columnasLadrillos = 9;
+var anchoLadrillo = 45;
+var alturaLadrillo = 15;
+var huecoLadrillos = 10;
+var marginTop = 10;
+var marginLeft = 8;
 document.addEventListener("keydown", pulsa, false);
 document.addEventListener("keyup", suelta, false);
+
+var ladrillos = [];
+for(c=0; c<columnasLadrillos; c++) {
+    ladrillos[c] = [];
+    for(f=0; f<filasLadrillos; f++) {
+        ladrillos[c][f] = { x: 0, y: 0, estado: 1 };
+    }
+}
 
 function pulsa (e) { 
     if (e.keyCode == 39) {
@@ -42,6 +56,7 @@ function bola() {
     ctx.fill()
     ctx.closePath();
 }
+
 function Fbase () {
     ctx.beginPath();
     ctx.rect(posicionBase, canvas.height-alturaBase, anchoBase, alturaBase);
@@ -49,17 +64,62 @@ function Fbase () {
     ctx.fill();
     ctx.closePath();
 }
+function ladrillo() {
+    for(c=0; c<columnasLadrillos; c++) {
+        for(f=0; f<filasLadrillos; f++) {
+            if(ladrillos[c][f].estado == 1) {
+                var ladrilloX = (c*(anchoLadrillo+huecoLadrillos))+marginLeft;
+                var ladrilloY = (f*(alturaLadrillo+huecoLadrillos))+marginTop;
+                ladrillos[c][f].x = ladrilloX;
+                ladrillos[c][f].y = ladrilloY;
+                ctx.beginPath();
+                ctx.rect(ladrilloX, ladrilloY, anchoLadrillo, alturaLadrillo);
+                ctx.fillStyle = "grey";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function colision() {
+    for(c=0; c<columnasLadrillos; c++) {
+        for(f=0; f<filasLadrillos; f++) {
+            var b = bricks[c][f];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+anchoLadrillo && y > b.y && y < b.y+alturaLadrillo) {
+                    vely = -vely;
+                    b.estadi = 0;
+                }
+            }
+        }
+    }
+}
+
 
 function dibuja (){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ladrillo();
     bola();
     Fbase();
+    colision();
 
     if (x >= (canvas.width - radio) || x <= radio) {
         velx = -velx;
     }
-    if (y >= (canvas.height - radio) || y <= radio) {
+    if (y <= radio) {
         vely = -vely;
+    }
+    else if (y >= (canvas.height - radio)){
+        if(x > posicionBase && x < posicionBase + anchoBase) {
+            if(y= y- alturaBase){
+                vely = -vely;
+            }
+        }
+        else {
+            alert("HAS PERDIDO");
+            document.location.reload();
+        }
     }
 
     if (pulsacionDerecha && posicionBase < canvas.width-anchoBase) {
@@ -73,4 +133,4 @@ function dibuja (){
     y = y + vely;
 }
 
-setInterval(dibuja, 1)
+setInterval(dibuja, 5)
